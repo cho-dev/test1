e7ed523e0b344aec683f2d180b53683322f8f1e6
Coffey <cho-dev@users.noreply.github.com>
2016-01-06 20:43:44 +0900

    add: print_bar.c

--- hello.c
+++ hello.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
+void print_bar(void);
+
 int main(void) {
     printf("Hello World\n");
     printf("Welcome Cho-Dev on github\n");
     printf("add 1\n");
     printf("add 2\n");
+    print_bar();
     return 0;
 }
 
---
+++ print_bar.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void print_bar(void) {
+    printf("PRINT_BAR\n");
+}
