5e34ec01f1fc359bbf330b6561af5a7438bcd2b6
Coffey <cho-dev@users.noreply.github.com>
2016-01-06 20:33:48 +0900

    add print_test.c

--- hello.c
+++ hello.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
 
+void print_test1(void);
+
 int main(void) {
     printf("Hello World\n");
     printf("Welcome Cho-Dev on github\n");
     printf("add 1\n");
     printf("add 2\n");
+    print_test1();
     return 0;
 }
 
---
+++ print_test.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+
+void print_test1(void) {
+    printf("PRINT_TEST1\n");
+}
